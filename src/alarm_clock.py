f8b270dfa50b593c7e31424fe52ca9a332cf88c2
Ryan Fehr <coderry4@gmail.com>
2025-08-20 20:22:40 -0500

    Add conversion of minutes and seconds input from user and pass through to alarm function as total_seconds

--- src/alarm_clock.py
+++ src/alarm_clock.py
@@ -25,4 +25,7 @@ def alarm(seconds):
     while pygame.mixer.music.get_busy():
         pygame.time.Clock().tick(10)
 
-alarm(3)
+minutes = int(input("How many minutes to wait: "))
+seconds = int(input("How many seconds to wait: "))
+total_seconds = minutes * 60 + seconds
+alarm(total_seconds)
