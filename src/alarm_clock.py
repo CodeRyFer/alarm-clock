ee2983f9a5c90766b269de6be4717be7e7dfb4aa
Ryan Fehr <coderry4@gmail.com>
2025-08-20 20:20:04 -0500

    Add alarm sound to alarm function once countdown reaches zero

--- src/alarm_clock.py
+++ src/alarm_clock.py
@@ -7,6 +7,7 @@ CLEAR_AND_RETURN = "\033[H"
 def alarm(seconds):
     time_elapse = 0
 
+    print(CLEAR)
     while time_elapse < seconds:
         time.sleep(1)
         time_elapse += 1
@@ -15,5 +16,13 @@ def alarm(seconds):
         minutes_left = time_left // 60
         seconds_left = time_left % 60
 
-        print(f"{minutes_left:02d}:{seconds_left:02d}")
+        print(f"{CLEAR_AND_RETURN}{minutes_left:02d}:{seconds_left:02d}")
 
+    pygame.mixer.init()
+    pygame.mixer.music.load("alarm-clock/src/Alarm-Fast-High-Pitch-A3-www.fesliyanstudios.com.mp3")
+    pygame.mixer.music.play()
+
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
+
+alarm(3)
