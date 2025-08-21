a570b6c9377aec91bc39b93c9f002f46f8e77f59
Ryan Fehr <coderry4@gmail.com>
2025-08-20 20:09:49 -0500

    Add alarm function to count down seconds passed as argument

--- src/alarm_clock.py
+++ src/alarm_clock.py
@@ -1,4 +1,16 @@
 import pygame
+import time
 
+def alarm(seconds):
+    time_elapse = 0
 
+    while time_elapse < seconds:
+        time.sleep(1)
+        time_elapse += 1
+
+        time_left = seconds - time_elapse
+        minutes_left = time_left // 60
+        seconds_left = time_left % 60
+
+        print(f"{minutes_left}:{seconds_left}")
 
